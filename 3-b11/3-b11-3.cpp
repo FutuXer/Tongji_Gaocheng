1ce0d7da6a2507d197caf7b7438a62cff27a2bca
FutuXer <59602001@qq.com>
2025-03-18 09:25:53 +0800

    Add files via upload

---
+++ 3-b11/3-b11-3.cpp
@@ -0,0 +1,50 @@
+//2353726 ÐÅ01 ¸¶ìÏ³¬
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	int i, j, k, sum = 0;
+
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 2; j++) {
+			for (k = 0; k < 4; k++) {
+				cout << "*";
+				sum++;
+				if (k == 2)
+				{
+					continue;
+				}
+				if (k == 3)
+				{
+					break;
+				}
+				/************/
+			}
+			cout << ' ';
+			if (j == 1)
+			{
+				break;
+			}
+			/************/
+		}
+
+		cout << endl;
+		if (i == 1)
+		{
+			continue;
+		}
+		if (i == 2)
+		{
+			break;
+		}
+		/************/
+	}
+
+	cout << "sum=" << sum << endl;
+	cout << "i=" << i << endl;
+	cout << "j=" << j << endl;
+	cout << "k=" << k << endl;
+
+	return 0;
+}
